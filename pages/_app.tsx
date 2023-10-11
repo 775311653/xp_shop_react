88b1893452b99ef05b53b643f99cec06696891ff
xieping <775311653@qq.com>
2023-10-11 16:30:58 +0800

    1、header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import type { AppProps } from 'next/app';
-import { CacheProvider, EmotionCache } from '@emotion/react';
-import { ThemeProvider, CssBaseline, createTheme } from '@mui/material';
+import type {AppProps} from 'next/app';
+import {CacheProvider, EmotionCache} from '@emotion/react';
+import {ThemeProvider, CssBaseline, createTheme} from '@mui/material';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -12,8 +12,11 @@ import createEmotionCache from '../utility/createEmotionCache';
 import lightThemeOptions from '../styles/theme/lightThemeOptions';
 import '../styles/globals.css';
 import '../styles/common.scss';
+import Header from "@/pages/components/header/header";
+import css from './app.module.scss';
+
 interface MyAppProps extends AppProps {
-  emotionCache?: EmotionCache;
+    emotionCache?: EmotionCache;
 }
 
 const clientSideEmotionCache = createEmotionCache();
@@ -21,16 +24,19 @@ const clientSideEmotionCache = createEmotionCache();
 const lightTheme = createTheme(lightThemeOptions);
 
 const MyApp: React.FunctionComponent<MyAppProps> = (props) => {
-  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+    const {Component, emotionCache = clientSideEmotionCache, pageProps} = props;
 
-  return (
-    <CacheProvider value={emotionCache}>
-      <ThemeProvider theme={lightTheme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </CacheProvider>
-  );
+    return (
+        <CacheProvider value={emotionCache}>
+            <ThemeProvider theme={lightTheme}>
+                <CssBaseline/>
+                <div className={css.appContainer}>
+                    <Header/>
+                    <Component {...pageProps} />
+                </div>
+            </ThemeProvider>
+        </CacheProvider>
+    );
 };
 
 export default MyApp;
---
+++ pages/components/header/header.tsx
@@ -0,0 +1,67 @@
+import {observer} from "mobx-react";
+import {observable} from "mobx";
+
+import {useHistory, useLocation} from "react-router-dom";
+import {useEffect} from "react";
+import {useRouter} from 'next/router';
+import api from "@/api";
+import css from './header.module.scss';
+import cartPng from '@/public/brand/cart.png';
+import Image from "next/image";
+
+let commonUtils = require("@/utils/Common.js");
+let queryParams = {};
+let history;
+let router;
+let data = observable({
+    cart_list: [],
+    cart_total: 0,
+    product_list: [],
+});
+
+async function get_shop_cart_list() {
+    let res = await api.user.get_shop_cart_list();
+    if (res.code !== 0) return;
+
+    data.cart_list = res.result.data;
+    data.cart_total = res.result.total;
+}
+
+async function get_product_list() {
+    let res = await api.product.get_product_list();
+    if (res.code !== 0) return;
+
+    data.product_list = res.result.data;
+}
+
+function init(queryParams: {}) {
+    get_shop_cart_list();
+    get_product_list();
+}
+
+let Brand = observer(() => {
+    history = useHistory();
+    router = useRouter();
+    queryParams = router.query;
+    // console.log(JSON.parse(JSON.stringify(router)));
+
+    useEffect(() => {
+        init(queryParams);
+    }, []);
+
+    return (
+        <div className={css.container}>
+            <span className={css.title}>标题</span>
+            <div className={'flexGrow1'}></div>
+            <div className={css.cartBox}>
+                <div className={css.number}>12</div>
+                <div className={css.imgBox}>
+                    <Image src={'/brand/cart.png'} alt="" width={24} height={24} className={css.imgItem} />
+                </div>
+            </div>
+        </div>
+    )
+});
+
+export default Brand;
+
